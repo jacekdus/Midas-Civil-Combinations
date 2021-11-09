211dc62746e2d50c0d9480c820c392de78be451a
dus_j <jacekdus1990@gmail.com>
2021-11-09 09:00:24 +0100

    Resultant txt files now have comb-name prefix

--- App/Application.py
+++ App/Application.py
@@ -10,6 +10,7 @@ from App.Midas.MidasCombination import MidasCombination
 class Application:
     def __init__(self, main_load_combination: LoadCombination):
         self.comb_tree = CombinationTree(main_load_combination)
+        self.comb_name = main_load_combination.name
 
     def _get_prepared_data(self) -> List[MidasCombination]:
         comb_data = self.comb_tree.get_combination_data()
@@ -17,9 +18,13 @@ class Application:
         return Converter.comb_node_data_to_midas_combination(comb_data, self.comb_tree.name)
 
     def create_mct_command_file(self):
-        mct_command_log_file = MctCommandLogFile('mct_command.txt', self._get_prepared_data())
+        file_name = self.comb_name + '_mct_command.txt'
+
+        mct_command_log_file = MctCommandLogFile(file_name, self._get_prepared_data())
         mct_command_log_file.log()
 
     def create_log_file(self):
-        log_file = CombTreeLogFile('log.txt', self.comb_tree)
+        file_name = self.comb_name + '_log.txt'
+
+        log_file = CombTreeLogFile(file_name, self.comb_tree)
         log_file.log()
