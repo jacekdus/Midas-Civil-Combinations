e9ef8e79a384431740bbacccf081f3ed9a185092
dus_j <jacekdus1990@gmail.com>
2021-11-09 12:04:17 +0100

    Fixed Output directory

--- Main.py
+++ Main.py
@@ -7,5 +7,5 @@ app = Application(main_comb)
 app.create_mct_command_file()
 app.create_log_file()
 
-# This line of code merges txt files in 'App/Output' directory:
+# This line of code merges txt files in ./Output directory:
 merge_mct_command_files()
